8dcb58318fe5058a03875134d396d9cc720ff684
Jake Maynard <jntm1222@gmail.com>
2021-01-23 11:54:27 -0500

    deleting all the files . we enter the commit message for your changes. Lines starting

--- phase3phaseend/src/main/java/com/phase3phaseend/dao/TaskDAO.java
+++
@@ -1,9 +0,0 @@
-package com.phase3phaseend.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.phase3phaseend.entity.Task;
-
-public interface TaskDAO extends CrudRepository<Task, Integer> {
-
-}
--- phase3phaseend/src/main/java/com/phase3phaseend/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.phase3phaseend.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.phase3phaseend.entity.User;
-
-public interface UserDAO extends CrudRepository<User, Integer>{
-
-}
