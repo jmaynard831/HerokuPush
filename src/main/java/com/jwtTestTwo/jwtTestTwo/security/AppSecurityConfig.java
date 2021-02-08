505809ae6ec7c779259e35aec0e7db1bc22c20aa
Jake Maynard <jntm1222@gmail.com>
2021-02-08 13:37:40 -0500

    adding react paths to sec part

--- src/main/java/com/jwtTestTwo/jwtTestTwo/security/AppSecurityConfig.java
+++ src/main/java/com/jwtTestTwo/jwtTestTwo/security/AppSecurityConfig.java
@@ -57,7 +57,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
 	                .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig),JwtUsernameAndPasswordAuthenticationFilter.class)
 	                .authorizeRequests()
-	                .antMatchers("/**","/static/**","/manifest**", "index", "/css/**", "/js/**","/swagger-ui.html","/webjars/**").permitAll()
+	                .antMatchers("/","/static/**","/manifest**", "index", "/css/**", "/js/**","/swagger-ui.html","/webjars/**").permitAll()
 	                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
 	                .antMatchers("/register","/addUser","/error").permitAll()
 	                //Adding this comment
